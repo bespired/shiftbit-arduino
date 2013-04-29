04e4a5889851b04b6ebeeb0044f8e795f1a00a2d
bespired <joeri@bespired.com>
2013-04-29 11:32:17 +0200

    comments cleanup

--- shiftbit/shiftbit.ino
+++ shiftbit/shiftbit.ino
@@ -1,20 +1,18 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
  
-// #define LEDPIN 13
-
-
-//Pin connected to latch pin (ST_CP) of 74HC595
+// Check if your pins have the same connection:
+// Pin connected to latch pin (ST_CP) of 74HC595
 const int latchPin = 10;
-//Pin connected to clock pin (SH_CP) of 74HC595
+// Pin connected to clock pin (SH_CP) of 74HC595
 const int clockPin = 12;
-////Pin connected to Data in (DS) of 74HC595
+// Pin connected to Data in (DS) of 74HC595
 const int dataPin  = 11;
 
 // Indexed colors for the rgb LEDS
 // LEDS are blinking very fast to dim brightness and mix to colors.
-//                    frame1, frame2, frame3, frame4, frame5, x  extra byte so 
-//                    R G B 
+//                    frame1, frame2, frame3, frame4, frame5, extra byte. 
+//                    R G B   R G B   R G B   R G B   R G B   +
 const byte sets[] = { 0,0,0 , 0,0,0 , 0,0,0 , 0,0,0 , 0,0,0 , 0, // rgb's for off
                       0,0,0 , 1,1,1 , 0,0,0 , 1,0,1 , 0,0,0 , 1, // rgb's for white , not on much and a little less green.
                       1,0,0 , 0,0,0 , 1,0,0 , 0,0,0 , 1,0,0 , 2, // rgb's for red
@@ -24,7 +22,10 @@ const byte sets[] = { 0,0,0 , 0,0,0 , 0,0,0 , 0,0,0 , 0,0,0 , 0, // rgb's for of
                       0,0,0 , 1,1,0 , 0,0,0 , 1,1,0 , 0,0,0 , 6, // rgb's for geel
                       0,0,0 , 1,0,1 , 0,0,0 , 1,0,1 , 0,0,0 , 7, // rgb's for paars
                       0,0,0 , 0,1,1 , 0,0,0 , 0,1,1 , 0,0,0 , 8, // rgb's for cyaan
-}; 
+};
+// sets[] has 16 values in a row so we can do << 4 and not * 15,... thats all. 
+// we can also make a dimmed red by 1,0,0 ,0,0,0 ,0,0,0 ,0,0,0 ,0,0,0 = 1 red, 4 off.
+// addition could be to add a function that creates this set from 15bit RGB color ints.
 
 volatile byte colframe= 0;   // 5 'frames' of 
 volatile byte chip[15]= {};  // This setup has 3 595 chips and 5 'frames' to mix colors.
@@ -103,7 +104,7 @@ void setup() {
   digitalWrite(latchPin, HIGH);
   
   
- // pinMode(LEDPIN, OUTPUT);
+ 
  
     cli();          // disable global interrupts
     TCCR1A = 0;     // set entire TCCR1A register to 0
@@ -142,8 +143,8 @@ void setColors( byte led1, byte led2, byte led3, byte led4, byte led5, byte led6
   
   for ( byte c1 = 0; c1 < 5; c1++ )
   {
-    byte c2 = c1 + 5;
-    byte c3 = c2 + 5;
+    byte c2 = c1 + 5;  // fill array with values that can be blitterd to the shift chips
+    byte c3 = c2 + 5;  // chip 3 has array index 10-15, chip 2 has 5-9 and chip 1 has 0-4 for 5 'frames' of color mixing.
     byte mp = c1 * 3;
     idx = (led1 << 4) + mp; chip[ c1 ]  = sets[ idx + 2 ]     ; chip[ c1 ] |= sets[ idx + 1 ] << 1; chip[ c1 ] |= sets[ idx + 0 ] << 2;
     idx = (led2 << 4) + mp; chip[ c1 ] |= sets[ idx + 2 ] << 3; chip[ c1 ] |= sets[ idx + 1 ] << 4; chip[ c1 ] |= sets[ idx + 0 ] << 5;
@@ -153,7 +154,7 @@ void setColors( byte led1, byte led2, byte led3, byte led4, byte led5, byte led6
     idx = (led6 << 4) + mp; chip[ c2 ] |= sets[ idx + 2 ] << 7; chip[ c3 ] =  sets[ idx + 1 ] << 0; chip[ c3 ] |= sets[ idx + 0 ] << 1;
     idx = (led7 << 4) + mp; chip[ c3 ] |= sets[ idx + 2 ] << 2; chip[ c3 ] |= sets[ idx + 1 ] << 3; chip[ c3 ] |= sets[ idx + 0 ] << 4;
     idx = (led8 << 4) + mp; chip[ c3 ] |= sets[ idx + 2 ] << 5; chip[ c3 ] |= sets[ idx + 1 ] << 6; chip[ c3 ] |= sets[ idx + 0 ] << 7;
-  
+    
     chip[ c1 ]  = 255- chip[ c1 ]; // LEDS sink to Vcc
     chip[ c2 ]  = 255- chip[ c2 ]; //
     chip[ c3 ]  = 255- chip[ c3 ]; // 
@@ -166,8 +167,8 @@ void setColors( byte led1, byte led2, byte led3, byte led4, byte led5, byte led6
 void loop() {
   
   setColors( LEDS[anim+1], LEDS[anim+2], LEDS[anim+3], LEDS[anim+4], LEDS[anim+5], LEDS[anim+6], LEDS[anim+7], LEDS[anim+8] );
-  delay( LEDS[anim] * 10 );
-  anim+=9; if ( anim > frames*9 ) anim = 0;
+  delay( LEDS[anim] * 10 );                 // delay until next animation-frame, ISR will blitter the colorframes.
+  anim+=9; if ( anim > frames*9 ) anim = 0; // ( frames as is in animation-frames. )
 
 
 }
